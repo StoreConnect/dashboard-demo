4b38a61a71b5b8581085faa49d6e0df112f7592e
Jerome Demonchaux <jerome.demonchaux@gmail.com>
2018-06-18 15:36:00 +0200

    update time sync

--- POCStoreConnect/assets/js/timesync.js
+++ POCStoreConnect/assets/js/timesync.js
@@ -1,7 +1,9 @@
 let spanTS = document.querySelector("#timesync");
-
+fetch("http://apicapteur.westeurope.cloudapp.azure.com:3000/time_now").then(res => res.json()).then(function (response) {
+    spanTS.innerHTML = new moment(response.time_now).format("D/MM/YYYY HH:mm:s");
+});
 window.setInterval(function () {
     fetch("http://apicapteur.westeurope.cloudapp.azure.com:3000/time_now").then(res => res.json()).then(function (response) {
         spanTS.innerHTML = new moment(response.time_now).format("D/MM/YYYY HH:mm:s");
     });
-}, 500);
+}, 20000);
